1bdf9d14331ef459d302bc886382490e740d419b
Stefanus <stefanusdwichristianto@gmail.com>
2025-06-06 15:07:28 +0700

    update analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import ReactGA from "react-ga4";
+
 import Home from "./Home";
+// import komponen lain kalau ada
+
+ReactGA.initialize("G-N2B31V2XQG");
+
+// Komponen pelacak GA
+function GAListener() {
+  const location = useLocation();
+
+  useEffect(() => {
+    ReactGA.send({ hitType: "pageview", page: location.pathname });
+  }, [location]);
+
+  return null;
+}
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <Router>
+      <GAListener />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
